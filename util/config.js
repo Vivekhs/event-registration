f1837bbbdc2683378ecdc5484af8c938e9dedf99
vivekhs <viveksinha461@gmail.com>
2017-02-04 22:29:23 +0530

    image upload enpointurl changed

--- util/config.js
+++ util/config.js
@@ -1,2 +1,2 @@
 exports.connectionString = "mongodb://viveksinha:qwerty@ds139949.mlab.com:39949/eventregistrationdb";
-exports.endPointUrl = "http://localhost:4050";
+exports.endPointUrl = "https://warm-tundra-99054.herokuapp.com";
