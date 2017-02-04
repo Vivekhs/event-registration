107be476a70ef24cd9d0d6c4beb7cf713ae82c57
vivekhs <viveksinha461@gmail.com>
2017-02-04 20:59:45 +0530

    mongodb as service added

--- util/config.js
+++ util/config.js
@@ -1,2 +1,2 @@
-exports.connectionString = "mongodb://localhost:27017/Event";
+exports.connectionString = "mongodb://viveksinha:qwerty@ds139949.mlab.com:39949/eventregistrationdb";
 exports.endPointUrl = "http://localhost:4050";
